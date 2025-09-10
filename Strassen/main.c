844a3d030f067e52dd5d48f3a20fb12feb959597
h_tye <harryijtye@gmail.com>
2025-09-09 21:40:05 -0400

    Pre-recursion

--- Strassen/main.c
+++ Strassen/main.c
@@ -2,18 +2,28 @@
 
 /**
  * CODING STANDARDS:
- * 1. Avoid using complex libraries 
+ * 1. Avoid using complex libraries
  * 2. Arrays should be used instead of pointers where possible
  * 3. Structs are okay but should not have associated functions, i.e no methods
-*/
+ */
 
-
-int main(int dim1, int dim2) {
+int main(int dim1, int dim2)
+{
 
     // Define input array structure, temporaray until I/O implemented
     int *input_buffer = (int *)malloc(dim1 * dim2 * sizeof(int));
 
-    Matrix input_matrix = matrix_partition(input_buffer, dim1, dim2);
+    Matrix input_matrix = matrix_build(input_buffer, dim1, dim2);
 
+    Matrix partitioned_matrices[4]; // First 4 partitions of the matrix
+
+    for (int i = 1; i <= 4; i++)
+    {
+        partitioned_matrices[i - 1] = matrix_partition(input_matrix, dim1 / 2, dim2 / 2, i);
+    }
+
+    Matrix intermediates[7]; // Intermediate matrices for Strassen's algorithm, M values
+
+    // Enter recursion
+    calculate_intermediates(partitioned_matrices, intermediates);
 }
-
--- Strassen/main.h
+++ Strassen/main.h
@@ -7,7 +7,7 @@ typedef struct {
     int cols;
 } Matrix;
 
-Matrix matrix_partition(int *input_buffer, int dim1, int dim2) {
+Matrix matrix_build(int *input_buffer, int dim1, int dim2) {
     Matrix mat;
     mat.rows = dim1;
     mat.cols = dim2;
@@ -26,4 +26,29 @@ Matrix matrix_partition(int *input_buffer, int dim1, int dim2) {
     }
 
     return mat;
+}
+
+Matrix matrix_partition(Matrix input_matrix, int new_rows, int new_cols, int quadrant) {
+
+    Matrix part;    
+    part.rows = new_rows;
+    part.cols = new_cols;
+
+    // Allocate memory for the partitioned matrix
+    part.matrix = (int **)malloc(new_rows * sizeof(int *));
+    for (int i = 0; i < new_rows; i++) {
+        part.matrix[i] = (int *)malloc(new_cols * sizeof(int));
+    }
+
+    // Populate new matrix based on quadrant
+    int row_offset = (quadrant / 2) * new_rows;
+    int col_offset = (quadrant % 2) * new_cols;
+
+    for (int i = 0; i < new_rows; i++) {
+        for (int j = 0; j < new_cols; j++) {
+            part.matrix[i][j] = input_matrix.matrix[i + row_offset][j + col_offset];
+        }
+    }
+
+    return part;
 }
