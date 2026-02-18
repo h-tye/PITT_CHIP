e0364ec3354f68ed77054243137ca5be7e4bd762
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-02-18 18:20:59 -0500

    Add FIFO_width parameter and update out_fields logic

--- HFT/FAST/Src/Field_FIFO.sv
+++ HFT/FAST/Src/Field_FIFO.sv
@@ -24,12 +24,13 @@ module Field_FIFO #(
     parameter num_decoders = 4,
     parameter beat_width = 64,
     parameter max_message_size = 10, 
-    parameter messageID_size = 21
+    parameter messageID_size = 21,
+    parameter FIFO_width = 2
     )(
         input logic clk,
         input logic rstn,
         input logic [1+messageID_size+$clog2(max_message_size)+beat_width:0] decoded_fields [0:num_decoders-1],
-        output logic [1+messageID_size+$clog2(max_message_size)+beat_width:0] out_fields [0:num_decoders-1]
+        output logic [1+messageID_size+$clog2(max_message_size)+beat_width:0] out_fields [0:num_decoders*FIFO_width-1]
     );
     
     reg [$clog2(2*num_decoders)+1+messageID_size+$clog2(max_message_size)+beat_width:0] registers [0:2*num_decoders-1];
@@ -38,19 +39,17 @@ module Field_FIFO #(
     
     always_ff @(posedge clk) begin
         if(!rstn) begin
-            for(k = 0; k < 2*num_decoders; k = k + 1) begin
+            for(k = 0; k < FIFO_width*num_decoders; k = k + 1) begin
                 registers[k] <= 0;
             end
             start_ptr <= 0;
         end
         else begin
-            for(k = start_ptr; k < start_ptr + num_decoders; k = k + 1) begin
+            for(k = 0; k < num_decoders*FIFO_width; k = k + 1) begin
                 out_fields[1+messageID_size+$clog2(max_message_size)+beat_width:0] <= registers[1+messageID_size+$clog2(max_message_size)+beat_width:0];
             end
-            if(start_ptr <= 0) begin
-                start_ptr <= 8;
-            end 
-            else begin
+            start_ptr <= start_ptr + num_decoders;
+            if(start_ptr == start_ptr*FIFO_width) begin
                 start_ptr <= 0;
             end
             
