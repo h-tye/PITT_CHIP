55cda8ff763c29d58bdabc8de7f9fc1504599a00
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-02-20 23:23:33 -0500

    Refactor MemoryCtrl parameters and logic

--- HFT/FAST/Src/MemoryCtrl.sv
+++ HFT/FAST/Src/MemoryCtrl.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+=`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -23,45 +23,46 @@
 module MemoryCtrl #(
     parameter beat_width = 64,
     parameter num_templates = 4,
-    parameter template_field_size = 16, // Assume 16 bits of template info per field(datatype, operator)
+    parameter sup_paths = 4, // Number of superscalar paths
+    parameter template_field_size = 10, // Assume 10 bits of template info per field(datatype, operator, optional, pmap, field num)
     parameter max_message_size = 10  // Max number of fields per template
     )(
     input logic clk,
     input logic rstn,
     input logic new_message,
-    input logic [beat_width+1:0] dins [0:3],
-    input logic [3:0] field_valids,
+    input logic [beat_width+1:0] dins [sup_paths],
+    input logic [sup_paths:0] field_valids,
     input logic [beat_width-1:0] replacement_field,         // To replace previous value
     input logic [$clog2(max_message_size)-1:0] replace_field_idx,
     input logic replace_field,
     output logic [$clog2(num_templates)-1:0] TID,
-    output logic [template_field_size-1:0] out_template [max_message_size-1:0],
-    output logic [beat_width-1:0] out_previous [max_message_size-1:0]
+    output logic [template_field_size-1:0] out_template [max_message_size],
+    output logic [beat_width-1:0] out_previous [max_message_size]
     );
     
     
     // Templates storage and previous value storage
-    reg [template_field_size-1:0] template [num_templates-1:0][max_message_size-1:0];
+    reg [template_field_size-1:0] template [num_templates-1:0][max_message_size-1:0]; // ROM
     reg [beat_width-1:0] previous_values [num_templates-1:0][max_message_size-1:0];
-    reg [$clog2(num_templates)-1:0] current_template;
 
     integer k;
     always_ff @(posedge clk) begin
         // Update temaplate and previous values for new message
         if(new_message) begin
-            for(k = 0; k < num_templates; k = k + 1) begin
-                if(dins[beat_width]) begin
+            for(k = 0; k < sup_paths; k = k + 1) begin
+                if(dins[beat_width] && field_valids[k]) begin
                     TID <= dins[k];
-                    out_template <= template[k];
-                    out_previous <= previous_values[k];
-                    current_template <= k;
+                    if(TID != dins[k]) begin
+                        out_template <= template[k];       // Only fetch if TID is different
+                        out_previous <= previous_values[k];
+                    end
                 end
-            end
+            end        
         end
         // Replace previous value based on decoding logic
         if(replace_field) begin
-            previous_values[current_template][replace_field_idx] <= replacement_field[replace_field_idx];
-         end
+            previous_values[TID][replace_field_idx] <= replacement_field[replace_field_idx];
+        end
     end 
     
     
