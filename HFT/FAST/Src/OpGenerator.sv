8241c4d0dd21f466c16c762eb5f90f4cf9f1e55b
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-02-23 12:58:21 -0500

    Add new output fields for t_field_ops_stream and t_field_ops_prev

--- HFT/FAST/Src/OpGenerator.sv
+++ HFT/FAST/Src/OpGenerator.sv
@@ -32,43 +32,89 @@ module OpGenerator #(
     input logic clk,
     input logic [template_field_size-1:0] template [max_message_size-1:0],
     input logic [beat_width-1:0] pmap,
-    output logic [field_op_size-1:0] field_ops [max_message_size-1:0]
+    output logic [field_op_size-1:0] t_field_ops_stream [max_message_size-1:0],
+    output logic [field_op_size-1:0] t_field_ops_prev [max_message_size-1:0]
     );
     
-    integer k;
+    integer k, prev_count, stream_count;
     reg [20:0] message_count; 
+    
     always_comb begin
         if(~rstn) begin
             for(k = 0; k < max_message_size; k = k + 1) begin
-                field_ops[k] = 0;
+                t_field_ops_stream[k] = 0;
+                t_field_ops_prev[k] = 0;
             end
         end
         else if(new_message) begin
             for(k = 0; k < max_message_size; k = k + 1) begin
-                field_ops[k][template_field_size-1:0] = template[k];
-                field_ops[k][field_op_size-2:template_field_size] = message_count;
+                
+                t_field_ops_stream[prev_count][field_op_size-2:template_field_size] = message_count;
+                t_field_ops_prev[stream_count][field_op_size-2:template_field_size] = message_count;
                 
                 // Set mem write to replace based on pmap and operator, see documentation
+                
+                // No Operator
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 0) begin
-                    field_ops[k][field_op_size-1] = 0;  // No Operator
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 0;
+                    stream_count = stream_count + 1;
                 end
+                
+                // Constant Operator
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 1) begin
-                    field_ops[k][field_op_size-1] = 0;  // Constant Operator
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 0; 
+                    stream_count = stream_count + 1;
                 end
+                
+                // Copy operator
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 2 && pmap[k] == 1) begin
-                    field_ops[k][field_op_size-1] = 1;  // Copy operator
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 1;  
+                    stream_count = stream_count + 1;
                 end
+                else if(template[k][field_op_size-1 -: $clog2(num_templates)] == 2) begin
+                    t_field_ops_prev[k][template_field_size-1:0] = template[k];
+                    t_field_ops_prev[k][field_op_size-1] = 0; 
+                end
+                    
+                // Default operator
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 3) begin
-                    field_ops[k][field_op_size-1] = 0;  // Default operator
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 0;
+                    stream_count = stream_count + 1;
                 end
+                
+                // Delta operator, always write back, always present
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 4) begin
-                    field_ops[k][field_op_size-1] = 1;  // Delta operator, always write back
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 1;
+                    stream_count = stream_count + 1;
                 end
+                
+                // Increment operator
                 if (template[k][field_op_size-1 -: $clog2(num_templates)] == 5 && pmap[k] == 1) begin
-                    field_ops[k][field_op_size-1] = 1;  // Increment operator
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 1;
+                    stream_count = stream_count + 1;
                 end
-                if (template[k][field_op_size-1 -: $clog2(num_templates)] == 7 && pmap[k] == 1) begin
-                    field_ops[k][field_op_size-1] = 1;  // Default operator
+                else if(template[k][field_op_size-1 -: $clog2(num_templates)] == 5) begin
+                    t_field_ops_prev[k][template_field_size-1:0] = template[k];
+                    t_field_ops_prev[k][field_op_size-1] = 0; 
+                    prev_count = prev_count + 1;
+                end
+                
+                // Tail operator
+                if (template[k][field_op_size-1 -: $clog2(num_templates)] == 6 && pmap[k] == 1) begin
+                    t_field_ops_stream[stream_count][template_field_size-1:0] = template[k];
+                    t_field_ops_stream[stream_count][field_op_size-1] = 1;
+                    stream_count = stream_count + 1;
+                end
+                else if(template[k][field_op_size-1 -: $clog2(num_templates)] == 6) begin
+                    t_field_ops_prev[prev_count][template_field_size-1:0] = template[k];
+                    t_field_ops_prev[prev_count][field_op_size-1] = 0; 
+                    prev_count = prev_count + 1;
                 end
                 
             end
