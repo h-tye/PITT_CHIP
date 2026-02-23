542f54c1c44c322d657cec501b8a52dd0afd258a
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-02-23 12:59:11 -0500

    Increase number of decoders from 4 to 8

--- HFT/FAST/Src/DecoderCtrl.sv
+++ HFT/FAST/Src/DecoderCtrl.sv
@@ -26,26 +26,25 @@ module DecoderCtrl #(
     parameter sup_paths = 4,
     parameter template_field_size = 10, // Assume 10 bits of template info per field(datatype, operator, optional, pmap, field num)
     parameter max_message_size = 10,  // Max number of fields per template
-    parameter num_decoders = 4,
+    parameter num_decoders = 8,  
     parameter field_op_size = 32   // Assume 32 bits for ops per field
     )(
     input logic clk,
     input logic rstn,
     input logic new_message,
-    input logic [num_decoders-1:0] decoders_done,
+    input logic [num_decoders/2-1:0] prev_decoders_done,
     input logic [template_field_size-1:0] template [max_message_size],
     input logic [beat_width+1:0] dins [sup_paths],
-    input logic [sup_paths-1:0] field_valid,
+    input logic [sup_paths-1:0] field_valids,
+    input logic [sup_paths-1:0] field_complete,
     input logic [$clog2(num_templates)-1:0] TID,
-    output logic [field_op_size-1:0] field_ops[num_decoders]
+    output logic [field_op_size-1:0] field_ops_stream[num_decoders/2],
+    output logic [field_op_size-1:0] field_ops_prev[num_decoders/2]
     );
     
-    logic [beat_width-1:0] pmap;
-    logic [$clog2(num_decoders)-1:0] current_batch_completed;
-    logic [field_op_size-1:0] field_ops_total [max_message_size-1:0]; // extra 2 bits for issued and completion
-    logic [max_message_size-1:0] fields_issued;
-    logic [max_message_size-1:0] fields_completed;
-    logic [$clog2(max_message_size)-1:0] ops_order [0:num_decoders-1];
+    logic [beat_width-1:0] pmap, pmap_reg;
+    logic [field_op_size-1:0] t_field_ops_stream [max_message_size-1:0];
+    logic [field_op_size-1:0] t_field_ops_prev [max_message_size-1:0];
     integer k,j;
     
     OpGenerator U(
@@ -53,59 +52,42 @@ module DecoderCtrl #(
         .rstn(rstn),
         .template(template),
         .pmap(pmap),
-        .field_ops(field_ops_total)
+        .field_ops_stream(t_field_ops_stream),
+        .field_ops_prev(t_field_ops_prev)
+        );
+        
+     OpScheduler StreamScheduler(
+        .clk(clk),
+        .rstn(rstn),
+        .field_valids(field_complete),
+        .total_field_ops(t_field_ops_stream),
+        .field_ops(field_ops_stream)
+        );
+        
+     OpScheduler PrevScheduler(
+        .clk(clk),
+        .rstn(rstn),
+        .field_valids(prev_decoders_done),
+        .total_field_ops(t_field_ops_prev),
+        .field_ops(field_ops_prev)
         );
      
      // Set pmap
+     always_comb begin
+        for(k = 0; k < sup_paths; k = k + 1) begin
+            if(dins[k][beat_width+1] && field_complete[k]) begin
+                pmap <= dins[k][beat_width-1:0];
+            end
+        end
+     end
+            
      always_ff @(posedge clk) begin
         if(!rstn) begin
             pmap <= 0;
         end
         else if(new_message) begin
-            for(k = 0; k < sup_paths; k = k + 1) begin
-                if(dins[k][beat_width+1] && field_valid[k]) begin
-                    pmap <= dins[k][beat_width-1:0];
-                end
-            end
+            pmap_reg <= pmap;
         end
      end
-     
-    // Issue new ops based on completion
-    integer field_count = 0;
-    always_ff @(posedge clk) begin
-        for(j = 0; j < num_decoders; j = j + 1) begin
-            if(decoders_done[k]) begin
-                fields_completed[ops_order[k]] <= 1;
-            end
-            else begin
-                fields_completed[ops_order[k]] <= 0;
-            end
-        end
-        
-        for(k = 0; k < max_message_size ; k = k + 1) begin 
-            if(!fields_completed[k] && field_count < num_decoders) begin // If not completed, reissue
-                field_ops[ops_order[k]] <= field_ops_total[k];
-                field_count = field_count + 1;
-            end
-        end
-    end
-    
-    integer start_ptr;
-    always_ff @(posedge clk) begin
-        if(!rstn) begin
-            for(k = 0; k < num_decoders; k = k + 1) begin
-                ops_order[k] <= k; // Intialize to 0,1,2,3, etc. 
-            end
-            start_ptr <= num_decoders; // Point to next starting field
-        end
-        else begin
-            for(k = 0; k < num_decoders; k = k + 1) begin
-                if(decoders_done[k]) begin
-                    ops_order[k] <= start_ptr;
-                    start_ptr <= start_ptr + 1;
-                end
-             end
-        end
-    end
          
 endmodule
