76c68a4a5e0a3f17b818f99d57f1bf8e4987f53a
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-03-06 21:48:19 -0500

    Update PresenceCalc_Enc module parameters and logic

--- HFT/FAST/Src/Encoder/PresenceCalc_Enc.sv
+++ HFT/FAST/Src/Encoder/PresenceCalc_Enc.sv
@@ -22,9 +22,10 @@
 
 module PresenceCalc_Enc #(
     parameter orders_generated = 4,
+    parameter dynamic_fields = 4, // Side, Type, Price, Qty
     parameter price_bits = 32,
     parameter qty_bits = 16,
-    parameter header_bits = 16,
+    parameter header_bits = 2, // Side bit, Order Type bit(Limit vs Market)
     parameter order_width = price_bits + qty_bits + header_bits,
     parameter max_message_size = 10, // Number of fields per message
     parameter max_delta = 16  // Max difference between previous and current value = 2^16 = 64k
@@ -33,26 +34,50 @@ module PresenceCalc_Enc #(
     input logic rstn,
     input logic [order_width-1:0] gen_orders [orders_generated],
     input logic [order_width-1:0] prev_orders [orders_generated],
-    output logic [1:0] PMAPs [orders_generated], // Presence only for price and qty
-    output logic [max_delta-1:0] encoded_values [orders_generated],
-    output logic [$clog2(max_delta)-1:0] encoded_size [0:1]
+    output logic [dynamic_fields-1:0] PMAPs [orders_generated], // Presence for side, type, price, and qty
+    output logic [header_bits+max_delta-1:0] encoded_values [orders_generated],
+    output logic [$clog2(max_delta)-1:0] encoded_size [0:1]  // Size only needed for price/qty, 1 bit for others
     );
     
     /**
       * ASSUME DELTA OPERATOR USED FOR PRICE AND QUANTITY
     */
     
-    // Modules for price
+    // Modules for side(Buy vs Sell)
     genvar i;
+    generate
+        for(i = 0; i < orders_generated; i = i + 1) begin
+                
+            DefCalc #(.input_bits(header_bits/2), .default_value(1)) Default_Side(
+                .order(gen_orders[i][order_width - 1]),
+                .present(PMAPs[i][0])
+                );
+            assign encoded_values[i][header_bits+max_delta-1] = gen_orders[i][order_width - 1];
+        end
+    endgenerate
+    
+    // Modules for order type(market vs limit)
+    generate
+        for(i = 0; i < orders_generated; i = i + 1) begin
+                
+            DefCalc #(.input_bits(header_bits/2), .default_value(1)) Default_Type(
+                .order(gen_orders[i][order_width - 2]),
+                .present(PMAPs[i][1])
+                );
+            assign encoded_values[i][header_bits+max_delta-2] = gen_orders[i][order_width - 2];
+        end
+    endgenerate
+    
+    // Modules for price
     generate
         for(i = 0; i < orders_generated; i = i + 1) begin
                 
             DeltaCalc #(.input_bits(price_bits)) Delta_Price(
                 .order(gen_orders[i][price_bits-1 -: price_bits]),
                 .prev_order(prev_orders[i][price_bits-1 -: price_bits]),
-                .present(PMAPs[i][0]),
+                .present(PMAPs[i][2]),
                 .encoded_value(encoded_values[i][price_bits-1 -: price_bits]),
-                .encoded_size(encoded_size[0])
+                .encoded_size(encoded_size[2])
                 );
         end
     endgenerate
@@ -64,9 +89,9 @@ module PresenceCalc_Enc #(
             DeltaCalc #(.input_bits(qty_bits)) Delta_Price(
                 .order(gen_orders[i][qty_bits-1 -: qty_bits]),
                 .prev_order(prev_orders[i][qty_bits-1 -: qty_bits]),
-                .present(PMAPs[i][1]),
+                .present(PMAPs[i][3]),
                 .encoded_value(encoded_values[i][qty_bits-1 -: qty_bits]),
-                .encoded_size(encoded_size[1])
+                .encoded_size(encoded_size[3])
                 );
         end
     endgenerate
