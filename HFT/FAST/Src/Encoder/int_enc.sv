bbe64955d51dc388680fb49009cda85c7086a1c0
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-03-23 12:54:35 -0400

    Add integer encoder module int_enc
    This module implements an integer encoder with configurable input and output widths, handling nullable inputs and encoding values accordingly.

---
+++ HFT/FAST/Src/Encoder/int_enc.sv
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/22/2026 07:57:00 PM
+// Design Name: 
+// Module Name: int_enc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module int_enc #(
+    parameter input_width = 64,
+    parameter output_width = input_width + input_width/8,
+    parameter output_bytes = output_width / 8 + 1
+    )(
+    input nullable,
+    input logic [$clog2(input_width)-1:0] encoded_size,
+    input logic [input_width-1:0] op_encoded_value,
+    output logic [output_width-1:0] dt_encoded_value
+    );
+    
+    logic [output_width-1:0] temp;
+    always_comb begin
+        if(nullable && op_encoded_value > 0) begin
+            temp = op_encoded_value + 1; // Subtract 1
+        end
+        else begin
+            temp = op_encoded_value;
+        end
+    end
+    
+    always_comb begin
+        logic overflow;
+        int k;
+        overflow = 1'b0;
+        for (k = 0; k < output_bytes; k = k + 1) begin
+            overflow = temp[(k+1)*8];
+            if(k * 8 < encoded_size) begin
+                dt_encoded_value[(k+1)*8 -: 8] = {1'b0, op_encoded_value[(k+1)*8-1 -: 7]};
+            end
+            else begin
+                dt_encoded_value[(k+1)*8 -: 8] = {1'b1, op_encoded_value[(k+1)*8-1 -: 7]};
+            end
+            
+            temp = temp << 1;  // Shift over for stop bit
+            temp[(k+1)*8+1] = overflow; // Insert overflow bit
+        end
+    end
+endmodule
