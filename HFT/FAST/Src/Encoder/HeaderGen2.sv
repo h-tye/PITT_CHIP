b53d047986ac3bc89b75e2621676583aff006316
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-03-29 12:19:06 -0400

    Refactor HeaderGen2 parameters and input types

--- HFT/FAST/Src/Encoder/HeaderGen2.sv
+++ HFT/FAST/Src/Encoder/HeaderGen2.sv
@@ -26,19 +26,19 @@ module HeaderGen2 #(
     parameter max_delta = 16,
     parameter body_length_bits = max_delta,
     parameter checksum_bits = 8,
-    parameter FIX_msg_type_bits = 16,  // 2 bytes for FIX
-    parameter header2_bits = body_length_bits + checksum_bits + FIX_msg_type_bits,
+    parameter ord_type_bits = 8,
+    parameter side_bits = 8,
+    parameter header2_bits = body_length_bits + ord_type_bits + side_bits + checksum_bits,
     parameter header1_size_bytes = 2 + 1 + 4,
     parameter header2_size_bytes = header2_bits / 7 + 1,
     parameter qty_bytes =  (max_delta / 7 + 1),
     parameter price_bytes = (max_delta / 7 + 1)
     )(
-    input clk,
-    input rstn,
-    input [dynamic_fields-1:0] order_PMAPs [orders_generated],
-    input [body_length_bits-1:0] prev_body_length,
-    input [checksum_bits-1:0] prev_checksum,
-    output [header2_bits-1:0] header1 [orders_generated]
+    input logic [dynamic_fields-1:0] order_PMAPs [orders_generated],
+    input logic [body_length_bits-1:0] prev_body_length,
+    input logic [checksum_bits-1:0] prev_checksum,
+    input logic [1:0] type_bits [orders_generated],  // Buy vs Sell, Market vs Limit
+    output logic [header2_bits-1:0] header2 [orders_generated]
     );
     
     logic [body_length_bits-1:0] body_length [orders_generated];
@@ -62,6 +62,16 @@ module HeaderGen2 #(
                     body_length[i] = body_length[i] + qty_bytes;
                 end
                 checksum[i] = body_length[i][7:0]; // Checksum = totaly bytes % 256
+                
+                // Operator encode
+                header2[i][header2_bits-1 -: body_length_bits] = body_length[i] - prev_body_length;
+                if(type_bits[1]) begin
+                    header2[i][header2_bits-body_length_bits-1 -: 8] = 50; // Default = 0(buy), only encode if != 0, ASCII encode
+                end
+                if(!type_bits[0]) begin
+                    header2[i][header2_bits-body_length_bits-9 -: 8] = 49; // Default = 1(limit), only encode if != 1, AscII encode
+                end
+                header2[i][checksum_bits-1:0] = checksum[i] - prev_checksum;
              end
          end
      endgenerate
