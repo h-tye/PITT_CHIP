200199a1602811431b41dd7733acd6f861d849c8
Harry Tye <156721697+h-tye@users.noreply.github.com>
2026-04-06 15:42:26 -0400

    Refactor Matching_Core module for order processing

--- HFT/Hybrid_Memory/Matching_Core.sv
+++ HFT/Hybrid_Memory/Matching_Core.sv
@@ -21,7 +21,6 @@
 
 
 module Matching_Core #(
-    parameter aggr_cache_capacity = 128,
     parameter timestamp_bits = 32,
     parameter orderID_bits = 16,
     parameter price_bits = 16,
@@ -29,100 +28,53 @@ module Matching_Core #(
     parameter order_width = timestamp_bits + orderID_bits + price_bits + qty_bits,
     parameter num_incoming_orders = 1
     )(
-    input logic [qty_bits-1:0] prev_qty,
-    input logic prev_BoS,                     // Signal wether previous is sell or buy overflow
     input logic [price_bits-1:0] buy_price,
     input logic [price_bits-1:0] sell_price,
     input logic [qty_bits-1:0] buy_qty,
     input logic [qty_bits-1:0] sell_qty,
-    output logic [qty_bits-1:0] next_qty,
-    output logic next_BoS,
-    output logic filled,                 // Indicate filled or not
-    output logic partially_filled
+    output logic [qty_bits-1:0] next_buy_qty,
+    output logic [qty_bits-1:0] next_sell_qty,
+    output logic buy_filled,
+    output logic sell_filled,
+    output logic buy_pfilled,
+    output logic sell_pfilled
     );
 
-    // Handle overflow first
-    logic [qty_bits-1:0] active_sell_qty, active_buy_qty;
-    always_comb begin
-        active_sell_qty = sell_qty;
-        active_buy_qty = buy_qty;
-        if(!prev_BoS) begin // Buy side overflow
 
-            // If overflow >= sell qty, mark as filled and move overflow to next core
-            if(prev_qty >= sell_qty) begin
-                filled = 1;
-                partially_filled = 1;
-                next_BoS = 0;
-                next_qty = prev_qty - sell_qty;
+    always_comb begin
+        if(buy_price >= sell_price) begin
+            if(buy_qty > sell_qty) begin
+                next_buy_qty = buy_qty - sell_qty;
+                next_sell_qty = 0;
+                buy_pfilled = 1;
+                buy_filled = 0;
+                sell_pfilled = 1;
+                sell_filled = 0;
+            end
+            else if(sell_qty > buy_qty) begin
+                next_buy_qty = 0;
+                next_sell_qty = sell_qty - buy_qty;
+                buy_pfilled = 0;
+                buy_filled = 1;
+                sell_pfilled = 0;
+                sell_filled = 1;
             end
             else begin
-                active_sell_qty = sell_qty - prev_qty;
-                // Once overflow dealt with, can handle current orders
-                if(buy_price >= sell_price) begin
-                    if(active_buy_qty > active_sell_qty) begin  // Buy overflow
-                        next_qty = active_buy_qty - active_sell_qty;
-                        next_BoS = 0;
-                        filled = 1;
-                        partially_filled = 1;
-                    end
-                    else if(active_buy_qty < active_sell_qty)  begin // Sell overflow
-                        next_qty = active_sell_qty - active_buy_qty;
-                        next_BoS = 1;
-                        filled = 1;
-                        partially_filled = 1;
-                    end
-                    else begin  // Perfect match
-                        next_qty = 0;
-                        next_BoS = 0; // default, will be ignored since filled = 0
-                        filled = 1;
-                        partially_filled = 0;
-                    end
-                end
-                else begin
-                    next_qty = 0;
-                    next_BoS = 0;
-                    filled = 0;
-                    partially_filled = 0;
-                end
+                next_buy_qty = 0;
+                next_sell_qty = 0;
+                buy_pfilled = 0;
+                buy_filled = 1;
+                sell_pfilled = 0;
+                sell_filled = 1;
             end
         end
-        else begin // Sell side overflow
-            if(prev_qty >= buy_qty) begin
-                filled = 1;
-                next_BoS = 1;
-                next_qty = prev_qty - buy_qty;
-                partially_filled = 1;
-            end
-            else begin
-                active_buy_qty = buy_qty - prev_qty;
-                // Once overflow dealt with, can handle current orders
-                if(buy_price >= sell_price) begin
-                    if(active_buy_qty > active_sell_qty) begin  // Buy overflow
-                        next_qty = active_buy_qty - active_sell_qty;
-                        next_BoS = 0;
-                        filled = 1;
-                        partially_filled = 1;
-                    end
-                    else if(active_buy_qty < active_sell_qty)  begin // Sell overflow
-                        next_qty = active_sell_qty - active_buy_qty;
-                        next_BoS = 1;
-                        filled = 1;
-                        partially_filled = 1;
-                    end
-                    else begin  // Perfect match
-                        next_qty = 0;
-                        next_BoS = 0; // default, will be ignored since filled = 0
-                        filled = 1;
-                        partially_filled = 0;
-                    end
-                end
-                else begin
-                    next_qty = 0;
-                    next_BoS = 0;
-                    filled = 0;
-                    partially_filled = 0;
-                end
-            end
+        else begin
+            next_buy_qty = buy_qty;
+            next_sell_qty = sell_qty;
+            buy_pfilled = 0;
+            buy_filled = 0;
+            sell_pfilled = 0;
+            sell_filled = 0;
         end
     end
 endmodule
